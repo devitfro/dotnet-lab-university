d5badbac46f2161e75cce29c34fa7262077db3ae
devitfro <alinabogdanovaaaaa@gmail.com>
2026-05-27 18:12:16 +0300

    add lab07

---
+++ potapova_dotnet_2026/MedicalSystem/DoctorWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Core.Models;
+using System.Windows;
+
+namespace MedicalSystem
+{
+    public partial class DoctorWindow : Window
+    {
+        public Doctor Doctor { get; set; }
+
+        public DoctorWindow()
+        {
+            InitializeComponent();
+
+            Doctor = new Doctor();
+        }
+
+        public DoctorWindow(Doctor doctor)
+        {
+            InitializeComponent();
+
+            Doctor = doctor;
+
+            NameTextBox.Text = doctor.Name;
+            SpecializationTextBox.Text = doctor.Specialization;
+            ExperienceTextBox.Text =
+                doctor.ExperienceYears.ToString();
+
+            RatingTextBox.Text =
+                doctor.Rating.ToString();
+
+            AvailableCheckBox.IsChecked =
+                doctor.IsAvailable;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Doctor.Name = NameTextBox.Text;
+            Doctor.Specialization =
+                SpecializationTextBox.Text;
+
+            Doctor.ExperienceYears =
+                int.Parse(ExperienceTextBox.Text);
+
+            Doctor.Rating =
+                double.Parse(RatingTextBox.Text);
+
+            Doctor.IsAvailable =
+                AvailableCheckBox.IsChecked == true;
+
+            DialogResult = true;
+            Close();
+        }
+    }
+}
---
+++ potapova_dotnet_2026/MedicalSystem/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Core.Models;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace MedicalSystem
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Doctor> doctors;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            doctors = new ObservableCollection<Doctor>();
+
+
+            doctors.Add(new Doctor("Іваненко Іван", "Терапевт", 10, 4.5)
+            {
+                IsAvailable = true
+            });
+
+            doctors.Add(new Doctor("Петренко Ольга", "Кардіолог", 15, 4.8)
+            {
+                IsAvailable = false
+            });
+
+            DoctorsDataGrid.ItemsSource = doctors;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            DoctorWindow window = new DoctorWindow();
+
+            if (window.ShowDialog() == true)
+            {
+                doctors.Add(window.Doctor);
+            }
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Doctor selectedDoctor =
+                DoctorsDataGrid.SelectedItem as Doctor;
+
+            if (selectedDoctor == null)
+            {
+                MessageBox.Show("Оберіть лікаря");
+                return;
+            }
+
+            DoctorWindow window =
+                new DoctorWindow(selectedDoctor);
+
+            if (window.ShowDialog() == true)
+            {
+                DoctorsDataGrid.Items.Refresh();
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Doctor selectedDoctor =
+                DoctorsDataGrid.SelectedItem as Doctor;
+
+            if (selectedDoctor != null)
+            {
+                doctors.Remove(selectedDoctor);
+            }
+        }
+    }
+}
